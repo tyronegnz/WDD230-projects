859b66593364ee6e6bfffee3d3780806689279dd
tyronegnz <gon19026@byui.edu>
2020-10-11 00:12:13 -0600

    update

--- lesson4/js/responsive-menu.js
+++ lesson4/js/responsive-menu.js
@@ -1,5 +1,7 @@
-$ (document).ready(function(){
-    $("navigation").on("click", function(){
-        $("nav ul").toggleClass("open");
-    });
-});
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
